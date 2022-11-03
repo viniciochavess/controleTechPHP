a3d9a090cda725d206950b4b192a91fd65c24711
Vinicio Chaves <47008512+viniciochavess@users.noreply.github.com>
2022-11-03 17:00:56 -0300

    Delete receberDados.php

--- receberDados.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    $userName = filter_input(INPUT_POST, 'userName',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    $userKey = filter_input(INPUT_POST, 'userKey',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-
-    echo $userName . "   " . $userKey;
-
-
-?>
