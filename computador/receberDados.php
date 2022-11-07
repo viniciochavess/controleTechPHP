6531323576a51457325a2454be794034916b56d4
Vinicio Chaves <47008512+viniciochavess@users.noreply.github.com>
2022-11-07 10:18:17 -0300

    Update receberDados.php

--- computador/receberDados.php
+++ computador/receberDados.php
@@ -6,14 +6,14 @@ require("../bancoDados/conexaoPDO.php");
  $cpu = filter_input(INPUT_GET, 'cpu',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
  $memoria = filter_input(INPUT_GET, 'memoria',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
- $numberSeal = filter_input(INPUT_GET, 'numberSeal',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
  $anyDesk = filter_input(INPUT_GET, 'anyDesk',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+ $numberSeal = filter_input(INPUT_GET, 'numberSeal',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
  
  if($store && $sector && $memoria && $cpu && $numberSeal && $anyDesk){
 
     $sql = $pdo->prepare("INSERT into desktop (store,sector,cpu,memoria,anydesk,seal) values (?,?,?,?,?,?)");
-    $sql->execute(array($store,$sector,$cpu,$memoria,$numberSeal,$anyDesk));
-   
+    $sql->execute(array($store,$sector,$cpu,$memoria,$anyDesk,$numberSeal));
+ 
     header("location: viewer.php");
 
    
